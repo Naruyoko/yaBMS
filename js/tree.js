41f9fde0e53e455e57c5c60abf30ca119c78f3d2
Naruyoko <naru519372@gmail.com>
2021-03-22 01:00:16 -0500

    simple xlsx import

--- js/tree.js
+++ js/tree.js
@@ -101,23 +101,12 @@ Node.createDOM=function (id,matrix){
   return div;
 }
 Node.prototype.addChild=function (){
-  if (!this.matrix||this.matrix==Node.LEAST) return;
+  if (!this.matrix||this.matrix==Node.LEAST||this.childrenN>=1&&/\(0(,0)*\)$/.test(this.matrix)) return;
   var oldtop=document.getElementById("content"+this.id).getBoundingClientRect().top;
   if (this.childrenN==this.children.length){
-    var isStandardPath,expansion;
-    if (this.matrix==Node.LIMIT){
-      isStandardPath=true;
-      expansion=this.childrenN==0?"(0)(1)":"("+"0,".repeat(this.childrenN)+"0)("+"1,".repeat(this.childrenN)+"1)";
-    }else if (this.matrix=="(0)"){
-      if (this.childrenN>=1) return;
-      isStandardPath=true;
-      expansion=Node.LEAST;
-    }else{
-      var b=new Bms(this.matrix);
-      b.b=this.childrenN;
-      var newMatrix=b.expand().toStringWithEmptyRowRemoved().split("[")[0];
-      if (this.childrenN==0||this.children[this.childrenN-1].matrix!=newMatrix) expansion=newMatrix;
-      else return;
+    var expansion=betterExpand(this.matrix,this.childrenN);
+    var isStandardPath;
+    if (this.matrix!=Node.LIMIT&&this.matrix!="(0)"){
       var ancestorNode=this;
       while (ancestorNode){
         if (ancestorNode.matrix==Node.LIMIT){
@@ -125,12 +114,14 @@ Node.prototype.addChild=function (){
           break;
         }
         var p=ancestorNode.parent;
-        if (p&&p.children.indexOf(ancestorNode)>0&&Bms.lte(newMatrix,p.children[p.children.indexOf(ancestorNode)-1].matrix)){
+        if (p&&p.children.indexOf(ancestorNode)>0&&Bms.lte(expansion,p.children[p.children.indexOf(ancestorNode)-1].matrix)){
           isStandardPath=false;
           break;
         }
         ancestorNode=p;
       }
+    }else{
+      isStandardPath=true;
     }
     if (isStandardPath) this.children.push(Node(Node.nodeN++,expansion,this));
     else this.children.push(Node.mirror(Node.nodeN++,expansion,this));
@@ -212,7 +203,7 @@ Node.prototype.removeAnalysis=function (notationid){
   for (var i=0;i<notations.length;i++){
     if (data[notations[i]]!==undefined||data[notations[i]]!==null) break;
   }
-  if (i==notations.length) analysisData.set(this.matrix,null);
+  if (i==notations.length) analysisData.remove(this.matrix);
   this.updateAll();
 }
 Node.prototype.update=function (){
@@ -394,39 +385,62 @@ Node.toggleFocusOn=function (node){
   window.scrollBy(0,newtop-oldtop);
 }
 
-var searchByMatrix=function (matrix){
-  if (matrix===undefined) matrix=document.getElementById("searchByMatrixInput").value;
-  if (!matrix) matrix=Node.LEAST;
-  if (!/^(\u03b5|(\(\d+(,\d+)*\))+)$/.test(matrix)) return;
+var expansionCache={};
+var betterExpand=function (matrix,n){
+  var pairid=matrix+n;
+  if (expansionCache[pairid]!==undefined) return expansionCache[pairid];
+  var expansion;
+  if (matrix==Node.LEAST){
+    expansion=Node.LEAST;
+  }else if (matrix==Node.LIMIT){
+    expansion=n==0?"(0)(1)":"("+"0,".repeat(n)+"0)("+"1,".repeat(n)+"1)";
+  }else if (matrix=="(0)"){
+    expansion=Node.LEAST;
+  }else{
+    var b=new Bms(matrix);
+    b.b=n;
+    expansion=b.expand().toStringWithEmptyRowRemoved().split("[")[0];
+  }
+  expansionCache[pairid]=expansion;
+  return expansion;
+}
+var indexFromMatrix=function (matrix){
   var path;
   if (matrix==Node.LEAST){
     path=[0,0,0];
+  }else if (matrix==Node.LIMIT){
+    path=[];
   }else if (/^\(0(,0)*\)\(1(,1)*\)$/.test(matrix)){
     path=[matrix.split("(")[2].length/2-1];
-  }else{
+  }else if (/^(\(\d+(,\d+)*\))+$/.test(matrix)){
     var lastMatrix=Node.LIMIT;
     path=[];
     while (true){
       var expansion;
       var n=0;
       while (true){
-        if (lastMatrix==Node.LIMIT){
-          expansion=n==0?"(0)(1)":"("+"0,".repeat(n)+"0)("+"1,".repeat(n)+"1)";
-        }else{
-          var b=new Bms(lastMatrix);
-          b.b=n;
-          expansion=b.expand().toStringWithEmptyRowRemoved().split("[")[0];
-        }
+        expansion=betterExpand(lastMatrix,n);
         if (Bms.lte(matrix,expansion)) break;
-        if (new Bms(matrix).Lng()<=new Bms(expansion).Lng()) return;
+        if (new Bms(matrix).Lng()<=new Bms(expansion).Lng()){
+          console.warn("Nonstandard matrix: "+matrix);
+          return null;
+        }
         n++;
       }
       path.push(n);
       lastMatrix=expansion;
       if (Bms.eq(matrix,expansion)) break;
     }
+  }else{
+    console.error("Unable to parse matrix: "+matrix);
   }
-  searchByIndex(path);
+  return path;
+}
+var searchByMatrix=function (matrix){
+  if (matrix===undefined) matrix=document.getElementById("searchByMatrixInput").value;
+  if (!matrix) matrix=Node.LEAST;
+  var path=indexFromMatrix(matrix);
+  if (path) searchByIndex(path);
 }
 var searchByIndex=function (index){
   if (index===undefined) index=document.getElementById("searchByIndexInput").value;
@@ -434,7 +448,15 @@ var searchByIndex=function (index){
   if (typeof index=="string") index=index.split(",");
   var lastNode=Node.limitNode;
   for (var i=0;i<index.length;i++){
-    while (lastNode.childrenN<=index[i]) lastNode.addChild();
+    var expectedChildrenN=lastNode.childrenN;
+    while (expectedChildrenN<=index[i]){
+      lastNode.addChild();
+      expectedChildrenN++;
+    }
+    if (!lastNode.children[index[i]]){
+      console.error("Invalid index");
+      return;
+    }
     lastNode=lastNode.children[index[i]];
   }
   lastNode.node.scrollIntoView();
@@ -445,6 +467,119 @@ var highlightNode=function (node,time){
   if (time===undefined) time=800;
   node.classList.add("highlighted");
   setTimeout(function(){node.classList.remove("highlighted")},time);
+}
+
+var uploadFile=function (checkStandard){
+  if (analysisData.size>0&&!confirm("Uploading analysis overwrites the existing data. Are you sure you want to do that?")) return;
+  var fileInput=document.getElementById("uploadFile");
+  var fileExtention=fileInput.value.slice(fileInput.value.lastIndexOf(".")+1);
+  var reader=new FileReader();
+  if (fileExtention=="xls"||fileExtention=="xlsx"){
+    var reader=new FileReader();
+    reader.onload=function (e){
+      loadAnalysisFromXLS(e.target.result,checkStandard);
+    }
+    reader.readAsBinaryString(fileInput.files[0]);
+  }
+}
+var loadAnalysisFromXLS=function(fileData,checkStandard){
+  var workbook=XLSX.read(fileData,{type:"binary"});
+  var sheetNames=workbook.SheetNames;
+  var sheetsInOrder=[];
+  var firstHeaderOccurences={};
+  var firstHeaders=[];
+  for (var i=0;i<sheetNames.length;i++){
+    sheetsInOrder.push(workbook.Sheets[sheetNames[i]]);
+    var firstHeader=workbook.Sheets[sheetNames[i]]["A1"].v;
+    if (firstHeaders.indexOf(firstHeader)==-1){
+      firstHeaderOccurences[firstHeader]=1;
+      firstHeaders.push(firstHeader);
+    }else{
+      firstHeaderOccurences[firstHeader]++;
+    }
+  }
+  var mostCommonFirstHeader="";
+  var mostCommonFirstHeaderOccurences=0;
+  for (var i=0;i<firstHeaders.length;i++){
+    if (firstHeaderOccurences[firstHeaders[i]]>mostCommonFirstHeaderOccurences){
+      mostCommonFirstHeader=firstHeaders[i];
+      mostCommonFirstHeaderOccurences=firstHeaderOccurences[firstHeaders[i]];
+    }
+  }
+  var mainColName=prompt("Column for main notation?",mostCommonFirstHeader);
+  if (mainColName===null) return;
+  analysisData=new Map();
+  notations=[];
+  for (var i=0;i<sheetsInOrder.length;i++){
+    console.log("Working on: "+sheetNames[i]);
+    var sheet=sheetsInOrder[i];
+    var sheetKeys=Object.keys(sheet);
+    var mainCol;
+    var headers={};
+    var notationCols=[];
+    for (var j=0;j<sheetKeys.length&&/^[A-Z]+1$/.test(sheetKeys[j]);j++){
+      var col=sheetKeys[j].slice(0,-1);
+      if (sheet[sheetKeys[j]].v==mainColName||mainColName=="ForceA"&&col=="A") mainCol=col;
+      else notationCols.push(col);
+      headers[col]=sheet[sheetKeys[j]].v;
+    }
+    if (!mainCol) continue;
+    var maxRow=+sheet["!ref"].split(":")[1].replace(/[A-Z]/g,"");
+    for (var row=2;row<=maxRow;row++){
+      var matrixCell=sheet[mainCol+row];
+      if (!matrixCell) continue;
+      var matrix=matrixCell.v;
+      if (matrix.toLowerCase().indexOf("empty")!=-1) matrix=Node.LEAST;
+      while (matrix[matrix.length-1]==" ") matrix=matrix.slice(0,-1);
+      var data=analysisData.get(matrix);
+      if (data){
+        console.warn("Duplicate column: "+matrix);
+      }
+      if (checkStandard){
+        if (!indexFromMatrix(matrix)){
+          console.log("Nonstandard matrix found at '"+sheetNames[i]+"'!"+mainCol+row);
+        }
+      }
+      for (var j=0;j<notationCols.length;j++){
+        var analysisCell=sheet[notationCols[j]+row];
+        if (!analysisCell) continue;
+        var analysis=analysisCell.v;
+        if (!analysis) continue;
+        if (notations.indexOf(headers[notationCols[j]])==-1) notations.push(headers[notationCols[j]]);
+        if (!data){
+          data={};
+          analysisData.set(matrix,data);
+        }
+        data[headers[notationCols[j]]]=analysis;
+      }
+    }
+  }
+  console.log("Data loaded");
+  Node.updateAll();
+  console.log("Load complete");
+}
+var renameNotation=function(index,newname,force){
+  if (!force&&notations.indexOf(newname)!=-1){
+    console.error("Notation "+newname+" is already found.");
+    return;
+  }
+  if (index<0||index>=notations.length) throw Error("Index error.");
+  var oldname=notations[index];
+  if (notations.indexOf(newname)==-1){
+    notations.splice(index,1,newname);
+  }else{
+    notations.splice(index,1);
+  }
+  var analysisKeys=Array.from(analysisData.keys());
+  for (var i=0;i<analysisKeys.length;i++){
+    var matrix=analysisKeys[i];
+    var data=analysisData.get(matrix);
+    if (data[oldname]!==undefined&&data[oldname]!==null){
+      data[newname]=data[oldname];
+      data[oldname]=null;
+    }
+  }
+  Node.updateAll();
 }
 
 var notations=[];
